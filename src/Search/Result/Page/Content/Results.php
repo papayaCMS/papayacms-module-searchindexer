bf74b2dfea1df5795c35b175ed089b3361a6069a
Thomas Weinert <thomas@weinert.info>
2020-04-23 12:15:05 +0200

    [FIX] Load Snippets As HTML OR Text (Not XML)

--- src/Search/Result/Page/Content/Results.php
+++ src/Search/Result/Page/Content/Results.php
@@ -31,17 +31,31 @@ class PapayaModuleElasticsearchSearchResultPageContentResults {
           && isset($hit->highlight->content)
           && count($hit->highlight->content) > 0
       ) {
+        $document = new \Papaya\XML\Document();
         for ($i = 0; $i < count($hit->highlight->content); $i++) {
-          $fragment = $oneResult->appendElement('fragment');
-          $fragment->appendXml($hit->highlight->content[$i]);
+          try {
+            $html = $hit->highlight->content[$i];
+            if (trim($html) != '') {
+              @$document->loadHTML('<?xml encoding="UTF-8" ?>'.$hit->highlight->content[$i]);
+              $fragment = $oneResult->appendElement('fragment');
+              foreach ($document->xpath()->evaluate('//body/node()') as $node) {
+                $fragment->appendChild($fragment->ownerDocument->importNode($node, TRUE));
+              }
+            }
+          } catch (Exception $e) {
+          }
         }
       } elseif (preg_match('(^(.{1,200}\b))', $hit->_source->content, $match)) {
         $content = $match[1];
-        $oneResult->appendXml($content);
+        $oneResult->appendText($content);
       } else {
         $content = substr($hit->_source->content, 0, 200);
-        $oneResult->appendXml($content);
+        $oneResult->appendText($content);
       }
     }
+  }
+
+  function fragmentsToXML() {
+
   }
 }
